4476212c51884fd0edc10b892acbd67b118774d6
profclark <clarkm1@southernct.edu>
2021-04-10 15:14:23 -0400

    Exercise files

---
+++ README.md
@@ -0,0 +1,13 @@
+# Exercise: JavaScript Array/Function Practice
+In this exercise, you will practice implementing functions and manipulating arrays through a series of small function implementations.
+
+## Instructions
+An `index.html` file is included which includes the JavaScript in `js/app.js`. When `index.html` is loaded, the included JavaScript file will execute.
+
+Implement each of the 3 empty function declarations in `js/app.js` so that they behave as and return what is described in the comments above each respectively.
+
+To test out your implementation, console log the results of each function call with test scenarios at the top of `app.js` (comments indicate where to place function calls/output).
+
+Note that you will need to open up Chrome Dev Tools to see your output, as you will be logging everything to the console.
+
+This is not a graded assignment. We will go over my implementations on Tuesday.
---
+++ js/app.js
@@ -0,0 +1,51 @@
+"use strict";
+
+// Implement your console log statements and function calls here
+
+// i.e. console.log(combineAlternatingArrays([1, 2, 10], [15, 20, 30, 50]));
+
+
+
+/**
+ * Combines elements from two arrays into a single array. Elements should be inserted into the resulting array in alternating order, with the 1st element from the 1st array being inserted first, the 1st element from array 2 second, the 2nd element from array 1 third, and so forth.
+ * 
+ * For example, given the following ...
+ * 
+ * combineArraysAlt([1, 2, 10], [15, 20, 30, 50]);
+ * 
+ * The below array will be returned ...
+ * 
+ * [1, 15, 2, 20, 10, 30, 50]
+ */
+function combineAlernatingArrays(array1, array2) {
+
+    //Your function implementation
+}
+
+/**
+ * Rotates the array n times to the right. Elements that would be shifted off the end of the array are wrapped around from the beginning.
+ * 
+ * For example, given the following (rotate array 3 positions) ...
+ * rotateArray([1, 15, 2, 10, 3], 3)
+ * 
+ * The below array will be returned ...
+ * [2, 10, 3, 1, 15]
+ */
+function rotateArray(array, n) {
+
+    //Your function implementation
+}
+
+/**
+ * Choose random element from an array of elements and return.
+ * 
+ * For example, given the following ...
+ * chooseRandomElement(['John', 'Susan', 'Bill])
+ * 
+ * will return with equal probability one of elements in the list.
+ */
+function chooseRandomElement(array) {
+
+    //Your function implementation
+
+}
